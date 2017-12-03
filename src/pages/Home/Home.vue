338f1fe79101c73a96b1d010de9f9ed2db18e976
Juan Rodriguez <juan.j.rodriguez2@gmail.com>
2017-12-03 16:26:05 -0600

    first commit

---
+++ src/pages/Home/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container border">
+      <div class="header border4">HEADER</div>
+      <div class="menu border2">MENU</div>
+      <div class="content border3">
+      <h1>hello from ashleys site</h1>
+        
+        CONTENT</div>
+      <div class="footer border4">FOOTER</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+// import demographics from './modules/demographics'
+// import login from './modules/login'
+// import rewards from './modules/rewards'
+// import surveys from './modules/surveys'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({})
