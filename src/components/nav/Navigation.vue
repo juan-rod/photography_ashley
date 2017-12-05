4d313032422281e02722fa57f3113614b7d910c8
Juan Rodriguez <juan.j.rodriguez2@gmail.com>
2017-12-05 08:45:57 -0600

    worked on mobile responsive wireframe and add navigation

---
+++ src/components/nav/Navigation.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="nav">
+        <ul>
+            <li class="nav__item">Portfolio</li>
+            <li class="nav__item">The Experience</li>
+            <li class="nav__item">About</li>
+            <li class="nav__item">Blog</li>
+            <li class="nav__item">Contact</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'nav-bar',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="container border">
-      <div class="header border4">HEADER</div>
-      <div class="menu border2">MENU</div>
-      <div class="content border3">
-      <h1>hello from ashleys site</h1>
-        
-        CONTENT</div>
-      <div class="footer border4">FOOTER</div>
+  <div>
+    <div class="wrapper border">
+        <nav-bar class="header border4">HEADER Here</nav-bar>
+        <div class="menu border2">MENU</div>
+        <div class="content border3">CONTENT</div>
+        <div class="footer border4">FOOTER</div>
+    </div>
   </div>
 </template>
 
 <script>
+import NavBar from '../../components/nav/Navigation.vue'
 export default {
   name: 'HomeView',
+  components: { NavBar },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
